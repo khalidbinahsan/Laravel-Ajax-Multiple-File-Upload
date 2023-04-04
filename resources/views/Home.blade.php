a7ea34db89b064ca7146bbd6159291d59bd9b04a
Khalid Bin Ahsan <khalid.freelancerbd@gmail.com>
2023-04-05 00:32:01 +0600

    reinstall laravel and bootstrap scaffolding

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+@extends('Layout.app')
+@section('title', 'Laravel axios multiple file uploader')
+@section('content')
+    <dev class="container-fluid">
+        <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    <H4>Laravel axios multiple file uploader</H4>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </dev>
+@endsection
---
+++ resources/views/Layout/app.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+   <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    @yield('content')
+    <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
